b7163f356391aa66aa308d2e78e5858cf5230643
ManosGra <manosgrammos9@gmail.com>
2024-10-15 18:35:08 +0300

    aa

--- logos.php
+++ logos.php
@@ -5,14 +5,15 @@
         <div class="row align-items-center">
             <div class="col-md-4 text-center">
                 <a target="_blank" href="https://www.konami.com/en/"><img data-src="assets/konami.png" class="img-fluid"
-                    style="width:300px; height:250px;"></a>
-            </div>
-            <div class="col-md-4 text-center">
-                <a target="_blank" href="https://www.pokemon.com/us"><img data-src="assets/pokemon.png" class="img-fluid" style="width:300px; height:250px;"></a>
-            </div>
-            <div class="col-md-4 text-center">
-                <a target="_blank" href="https://en.bandainamcoent.eu/"><img data-src="assets/namco.jpg" class="img-fluid"
                         style="width:300px; height:250px;"></a>
+            </div>
+            <div class="col-md-4 text-center">
+                <a target="_blank" href="https://www.pokemon.com/us"><img data-src="assets/pokemon.png"
+                        class="img-fluid" style="width:300px; height:250px;"></a>
+            </div>
+            <div class="col-md-4 text-center">
+                <a target="_blank" href="https://en.bandainamcoent.eu/"><img data-src="assets/namco.jpg"
+                        class="img-fluid" style="width:300px; height:250px;"></a>
             </div>
         </div>
     </div>
