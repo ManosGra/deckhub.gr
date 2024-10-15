7b187cd17dc71064afc6f5f454cb263b2ee66979
ManosGra <manosgrammos9@gmail.com>
2024-10-15 18:09:16 +0300

    logos

---
+++ logos.php
@@ -0,0 +1,15 @@
+<section id="logos">
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-md-4">
+                <img data-src="assets/konami.png" class="img-fluid" style="width:420px; height:420px;">
+            </div>
+            <div class="col-md-4">
+                <img data-src="assets/konami.png" class="img-fluid" style="width:420px; height:420px;">
+            </div>
+            <div class="col-md-4">
+                <img data-src="assets/konami.png" class="img-fluid" style="width:420px; height:420px;">
+            </div>
+        </div>
+    </div>
+</section>
